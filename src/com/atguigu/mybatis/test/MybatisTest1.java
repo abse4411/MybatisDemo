7943a87dca6c4ca37ce98456ab11014606afe09c
Shen Zhifeng <shen_zhifeng@outlook.com>
2020-02-03 13:32:59 +0800

    add PageHelper

--- src/com/atguigu/mybatis/test/MybatisTest1.java
+++ src/com/atguigu/mybatis/test/MybatisTest1.java
@@ -3,6 +3,9 @@ package com.atguigu.mybatis.test;
 import com.atguigu.mybatis.bean.Department;
 import com.atguigu.mybatis.bean.Employee;
 import com.atguigu.mybatis.dao.*;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -26,7 +29,7 @@ public class MybatisTest1 {
                 +department.getDepartmentName()+"}");
     }
     public void print(Employee employee){
-        System.out.println("Employee{Id:"+employee.getdId()+",Name:"
+        System.out.println("Employee{Id:"+employee.getId()+",Name:"
                 +employee.getLastName()+",Email:"
                 +employee.getEmail()+",DId:"
                 +employee.getdId()+"}");
@@ -141,6 +144,49 @@ public class MybatisTest1 {
             Assert.fail();
         }finally {
 
+        }
+
+    }
+
+    @Test
+    public void testPageHelper() throws IOException {
+        SqlSessionFactory sqlSessionFactory=getSqlSessionFactory();
+        SqlSession session1=sqlSessionFactory.openSession();
+        try{
+            EmployeeMapperDynamicSql mapper1 = session1.getMapper(EmployeeMapperDynamicSql.class);
+            Page<Object> page = PageHelper.startPage(5, 3);
+            List<Employee> emps = mapper1.getEmpsByConditionChoose(null);
+            print(emps);
+//            System.out.println("当前页码："+page.getPageNum());
+//            System.out.println("总记录数："+page.getTotal());
+//            System.out.println("每页的记录数："+page.getPageSize());
+//            System.out.println("总页码："+page.getPages());
+
+
+
+            PageInfo<Employee> info = new PageInfo<>(emps, 5);
+			/*System.out.println("当前页码："+page.getPageNum());
+			System.out.println("总记录数："+page.getTotal());
+			System.out.println("每页的记录数："+page.getPageSize());
+			System.out.println("总页码："+page.getPages());*/
+            ///xxx
+            System.out.println("当前页码："+info.getPageNum());
+            System.out.println("总记录数："+info.getTotal());
+            System.out.println("每页的记录数："+info.getPageSize());
+            System.out.println("总页码："+info.getPages());
+            System.out.println("是否第一页："+info.isIsFirstPage());
+            System.out.println("连续显示的页码：");
+            int[] nums = info.getNavigatepageNums();
+            for (int i = 0; i < nums.length; i++) {
+                System.out.println(nums[i]);
+            }
+            Assert.assertTrue(emps.size()>0);
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+            Assert.fail();
+        }finally {
+            session1.close();
         }
 
     }
